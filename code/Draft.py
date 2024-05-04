1dc5f2fee2a635accf424a1548fd247e5bdb32c8
Daniil Kurganov <dk30128@gmail.com>
2024-05-04 20:55:45 +0300

    Реализация кодирования цвета пикселя

--- code/Draft.py
+++ code/Draft.py
@@ -2,7 +2,5 @@ import numpy as np
 from PIL import Image
 
 array_image = np.asarray(Image.open('C:/Users/User/PythonProjects/CascadeCodec/images/50.jpeg').convert('RGB'))
-print(array_image.shape)
-print(array_image[0].shape)
-print(array_image)
-Image.fromarray(array_image).save('result.jpeg')
+# Image.fromarray(array_image).save('result.jpeg')
+print(array_image.dtype)
