ed86e56214eec1e617fdd1bf1b8af381503c859e
Daniil Kurganov <dk30128@gmail.com>
2024-05-04 21:13:24 +0300

    Реализация кодирования исходного изображения кодом Хэмминга

--- code/Draft.py
+++ code/Draft.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
+from HammingCodec import hamming_codec_get_result
 
 array_image = np.asarray(Image.open('C:/Users/User/PythonProjects/CascadeCodec/images/50.jpeg').convert('RGB'))
-# Image.fromarray(array_image).save('result.jpeg')
-print(array_image.dtype)
+array_encode_image = np.vectorize(lambda uint8_item: hamming_codec_get_result(True, int(uint8_item)))(array_image)
+print(array_encode_image)
